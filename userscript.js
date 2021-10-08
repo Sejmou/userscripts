5795bef0b0e8c53483fd128475f850baaf785fff
Sejmou <samo.kolter@gmail.com>
2021-10-08 15:07:41 +0200

    oeticket-evt-bookmark: add event autoAdd; fixes, refactorings

--- userscript.js
+++ userscript.js
@@ -1,67 +1,89 @@
 // ==UserScript==
 // @name         oeticket event bookmarker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Samo Kolter
 // @match        https://www.oeticket.com/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addValueChangeListener
+// @noframes
 // ==/UserScript==
 
-'use strict';
+//Note: @noframes was necessary as the script fired multiple times for some reason?
 
-//if true, automatically adds the event to the bookmarks as soon as its page loads
+// ----------------- Config --------------
+//set to true if you want to remove all the events you have added before
+const clearEventStore = false;
+
+//if true, automatically adds the event of the current page to the bookmarks as soon as the page loads
 const autoAdd = true;
 
 
 
-const eventsLabel = 'events';
-const eventsArr = JSON.parse(GM_getValue(eventsLabel)) || [];
+// -------------------Code----------------
 
-console.log('eventsArr', eventsArr);
-console.log('mapped key value pairs', eventsArr.map(evObj => [evObj.id, evObj]));
-const eventsKeyValuePairs = eventsArr.map(event => [event.id, event]);
-const eventsMap = new Map(eventsKeyValuePairs);
+class EventStore {
+    //Note: apparently specifying properties with leading '#' makes them private (new JavaScript feature? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields )
+    #eventsLabel = 'events'; // the key under which this script stores the bookmarked events in the "GreaseMonkey storage"
+    #eventsMap;
 
-const artistName = document.querySelector('.stage-headline')?.innerText;
-const date = document.querySelector('time')?.innerText;
-const venueEl = document.querySelector('a[href="#venueInformation"]');
-const venue = venueEl?.innerText;
-
-const eventURLStr = 'https://www.oeticket.com/event/';
-const url = window.location.href;
-const onEventURL = window.location.href.indexOf(eventURLStr) === 0;
-const urlAfterEventURL = url.substring(eventURLStr.length);
-const eventId = onEventURL? urlAfterEventURL.substring(0, urlAfterEventURL.indexOf('/')) : undefined;
-
-const eventLink = eventId ? `${eventURLStr}${eventId}/` : undefined;
-const address = document.querySelector('.venue-address p')?.innerText.split('\n')[0].trim();
-const mapsLink = document.querySelector('.venue-address p a')?.href;
-
-
-const saveEventButton = document.createElement('button');
-
-function updateButtonText() {
-    saveEventButton.innerText = eventsMap.has(eventId) ? 'remove event from bookmarks' : 'add event to bookmarks';
-}
-
-saveEventButton.style.position = 'fixed';
-saveEventButton.style.top = '50px';
-saveEventButton.style.left = '20px';
-updateButtonText();
-
-
-saveEventButton.addEventListener('click', () => {
-    console.log('events before', eventsMap);
-
-    if (eventsMap.has(eventId)) {
-        eventsMap.delete(eventId);
-        console.log('removed event', eventId);
+    constructor() {
+        const eventsArr = JSON.parse(GM_getValue(this.#eventsLabel)) || [];
+        const eventsKeyValuePairs = eventsArr.map(event => [event.id, event]);
+        this.#eventsMap = new Map(eventsKeyValuePairs);
     }
-    else {
-        const eventObj = {
+
+    addEvent(eventObj) {
+        this.#eventsMap.set(eventObj.id, eventObj);
+        console.log(`added event with id ${eventObj.id}`, eventObj);
+
+        this.#updateEventsInGMStorage();
+    }
+
+    removeEvent(eventId) {
+        const elementRemoved = this.#eventsMap.delete(eventId);
+
+        if (elementRemoved) {
+            console.log('removed event', eventId);
+
+            this.#updateEventsInGMStorage();
+        }
+        else console.log(`no event with ID ${eventId} found`);
+    }
+
+    clear() {
+        this.#eventsMap = new Map();
+        GM_setValue(this.#eventsLabel, null);
+        console.log('cleared event store');
+    }
+
+    contains(eventId) {
+        return this.#eventsMap.has(eventId);
+    }
+
+    #updateEventsInGMStorage() {
+        GM_setValue(this.#eventsLabel, JSON.stringify(this.events));
+    }
+
+    get eventFromCurrentPage() {
+        const artistName = document.querySelector('.stage-headline')?.innerText;
+        const date = document.querySelector('time')?.innerText;
+        const venueEl = document.querySelector('a[href="#venueInformation"]');
+        const venue = venueEl?.innerText;
+
+        const eventURLStr = 'https://www.oeticket.com/event/';
+        const url = window.location.href;
+        const onEventURL = window.location.href.indexOf(eventURLStr) === 0;
+        const urlAfterEventURL = url.substring(eventURLStr.length);
+        const eventId = onEventURL ? urlAfterEventURL.substring(0, urlAfterEventURL.indexOf('/')) : undefined;
+
+        const eventLink = eventId ? `${eventURLStr}${eventId}/` : undefined;
+        const address = document.querySelector('.venue-address p')?.innerText.split('\n')[0].trim();
+        const mapsLink = document.querySelector('.venue-address p a')?.href;
+
+        return {
             id: eventId,
             link: eventLink,
             artistName,
@@ -70,66 +92,104 @@ saveEventButton.addEventListener('click', () => {
             address,
             mapsLink
         };
-
-        eventsMap.set(eventId, eventObj);
-        console.log(`added event with id ${eventId}`, eventObj);
     }
 
-    console.log('events after', eventsMap);
-
-    GM_setValue(eventsLabel, JSON.stringify(Array.from(eventsMap.values())));
-    updateButtonText();
-});
-
-
-
-function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
+    get events() {
+        return Array.from(this.#eventsMap.values());
+    }
 }
 
 
 
-const downloadButton = document.createElement('button');
-downloadButton.innerText = 'download events as CSV';
-downloadButton.style.position = 'fixed';
-downloadButton.style.top = '100px';
-downloadButton.style.left = '20px';
-
-downloadButton.addEventListener('click', () => {
-    const events = Array.from(eventsMap.values());
-    console.log(events);
-
-    if (events.length > 0) {
-        const eventPropNames = Object.getOwnPropertyNames(events[0]);
-        let str = '';
-        str += eventPropNames.join('#');
-        str += '\n';
-        const arrOfArrsOfEventPropVals = events.map(ev => eventPropNames.map(prop => ev[prop]));
-        str += arrOfArrsOfEventPropVals.map(evPropValArr => evPropValArr.join('#')).join('\n');
-        console.log(str);
-
-        download('events.csv', str);
-    }
-    else {
-        alert('You have not added any events to your bookmarks yet!');
-    }
-});
 
 
-if (eventId) {
-    document.body.appendChild(saveEventButton);
+
+
+function createEventBookmarkButton() {
+    const evtBookmarkBtn = document.createElement('button');
+    evtBookmarkBtn.style.position = 'fixed';
+    evtBookmarkBtn.style.top = '50px';
+    evtBookmarkBtn.style.left = '20px';
+    evtBookmarkBtn.innerText = eventStore.contains(eventStore.eventFromCurrentPage.id) ? 'remove event from bookmarks' : 'add event to bookmarks';
+
+
+    evtBookmarkBtn.addEventListener('click', () => {
+        console.log('events before', eventsMap);
+
+        if (eventsMap.has(eventStore.eventFromCurrentPage.id)) eventStore.removeEvent(eventStore.eventFromCurrentPage.id);
+        else eventStore.addEvent(eventStore.eventFromCurrentPage.id);
+
+        console.log('events after', eventsMap);
+        evtBookmarkBtn.innerText = eventsMap.has(eventStore.eventFromCurrentPage.id) ? 'remove event from bookmarks' : 'add event to bookmarks';
+    });
+
+    return evtBookmarkBtn;
+}
+
+
+
+
+
+
+
+function createDownloadButton() {
+    const downloadButton = document.createElement('button');
+    downloadButton.innerText = 'download events as CSV';
+    downloadButton.style.position = 'fixed';
+    downloadButton.style.top = '100px';
+    downloadButton.style.left = '20px';
+
+    downloadButton.addEventListener('click', () => {
+        const events = eventStore.events;
+        console.log(events);
+
+        if (events.length > 0) {
+            const eventPropNames = Object.getOwnPropertyNames(events[0]);
+            let str = '';
+            str += eventPropNames.join('#');
+            str += '\n';
+            const arrOfArrsOfEventPropVals = events.map(ev => eventPropNames.map(prop => ev[prop]));
+            str += arrOfArrsOfEventPropVals.map(evPropValArr => evPropValArr.join('#')).join('\n');
+            console.log(str);
+
+            download('events.csv', str);
+        }
+        else {
+            alert('You have not added any events to your bookmarks yet!');
+        }
+    });
+
+    return downloadButton;
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+
+
+
+
+
+
+const eventStore = new EventStore();
+if (clearEventStore) eventStore.clear();
+
+if (eventStore.eventFromCurrentPage?.id) {
+    if (autoAdd) eventStore.addEvent(eventStore.eventFromCurrentPage);
+    document.body.appendChild(createEventBookmarkButton());
 }
 else {
     console.warn('oeticket event bookmarker could not get event ID: Probably you are currently not on an event page');
 }
 
-document.body.appendChild(downloadButton);
+document.body.appendChild(createDownloadButton());
