a2f9783dc8e0f4a998040b9dd3082c5a4b328b2e
Sejmou <samo.kolter@gmail.com>
2022-08-06 15:41:48 +0200

    fetch-practice-tracks v 0.2: handle pagination + go to next song (if present)

--- fetch-practice-tracks.js
+++ fetch-practice-tracks.js
@@ -1,24 +1,20 @@
 // ==UserScript==
 // @name         Fetch Practice Track Video Links
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Fetches YouTube video links for all tracks of a given song page on musicalpracticetracks.com
+// @version      0.2
+// @description  Fetches YouTube video links for all tracks of a given Musical page on musicalpracticetracks.com
 // @author       You
 // @match        https://www.musicalpracticetracks.com/index.php/*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=musicalpracticetracks.com
 // @grant        none
 // ==/UserScript==
 
-function selectElements(selector) {
-  return Array.from(document.querySelectorAll(selector));
-}
-
-function selectElement(selector) {
-  return document.querySelector(selector);
+function selectElements(selector, parent = document) {
+  return Array.from(parent.querySelectorAll(selector));
 }
 
 function getTextContent(selector) {
-  return selectElement(selector).innerText;
+  return document.querySelector(selector).innerText;
 }
 
 function indexOfEnd(string, substring) {
@@ -26,14 +22,20 @@ function indexOfEnd(string, substring) {
   return io == -1 ? -1 : io + substring.length;
 }
 
+const pathName = window.location.pathname;
+const subpage = pathName.substring(indexOfEnd(pathName, 'index.php/'));
+const musicalId = subpage.split('/')[0];
+
 const songName = getTextContent('.entry-title');
 const songNr = songName.substring(0, songName.indexOf('.'));
-const videoPreviews = selectElements('.epyt-gallery-thumb');
-console.log(videoPreviews);
-const hasPagination = false; //TODO: detect and handle pagination
 
 const ytPlayerWrapper = document.querySelector('.fluid-width-video-wrapper');
 const ytVideoPlayer = ytPlayerWrapper.querySelector('iframe');
+
+const navContainer = document.querySelector('.entry-content p strong');
+const nextSongLink = navContainer.innerText.includes('Next:')
+  ? selectElements('a', navContainer).at(-1)
+  : null;
 
 let videoSrc = '';
 let videoTitle = '';
@@ -42,10 +44,11 @@ let titleChanged = false;
 
 const trackVideos = new Map();
 
-const previewIter = videoPreviews.values();
-previewIter.next().value; // iterator should actually start at second element - first is already handled by MutationObserver!
+let videoPreviews = selectElements('.epyt-gallery-thumb');
+let previewIter = videoPreviews.values();
+previewIter.next().value; // iterator should actually start at second element - first is already handled by ytPlayerObs!
 
-const obs = new MutationObserver(() => {
+const ytPlayerObs = new MutationObserver(() => {
   const { title, src } = ytVideoPlayer;
   if (videoSrc !== src) {
     videoSrc = src;
@@ -69,8 +72,36 @@ const obs = new MutationObserver(() => {
     if (!iterResult.done) {
       const nextPreview = iterResult.value;
       nextPreview.click(); // will cause this callback to retrigger once video player is loaded
+    } else {
+      const nextPageButton = document.querySelector('.epyt-next');
+      if (getComputedStyle(nextPageButton).visibility !== 'hidden') {
+        nextPageButton.click();
+      } else {
+        // done processing the page of this song!
+
+        if (nextSongLink) {
+          nextSongLink.click();
+        } else {
+          alert('DONE');
+        }
+      }
     }
   }
 });
 
-obs.observe(ytVideoPlayer, { attributes: true });
+const vidPreviewContainer = document.querySelector('.epyt-gallery-list');
+
+const vidPreviewContainerObs = new MutationObserver(() => {
+  videoPreviews = selectElements('.epyt-gallery-thumb');
+  previewIter = videoPreviews.values();
+  // click next video (== first of next page)
+  // -> will trigger MutationObserver once video changed
+  // -> it will store the next video's data and handle/click through rest of the videos
+  previewIter.next().value.click();
+});
+
+ytPlayerObs.observe(ytVideoPlayer, { attributes: true });
+vidPreviewContainerObs.observe(vidPreviewContainer, {
+  subtree: true,
+  childList: true,
+});
