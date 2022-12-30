4d04553691ddca192b00b95d50beb3100c230c93
Sejmou <samo.kolter@gmail.com>
2022-12-30 21:56:43 +0100

    youtube-subtitle-selector v0.2: should now work lol

--- youtube-subtitle-selector.js
+++ youtube-subtitle-selector.js
@@ -1,20 +1,42 @@
 // ==UserScript==
 // @name         YouTube subtitle selector
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Replace the commit timestamps on the 'commits' subpage of any repo with local time
 // @author       Sejmou
-// @match        https://www.youtube.com/watch/*
+// @match        https://www.youtube.com/watch*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @downloadURL  https://raw.githubusercontent.com/Sejmou/userscripts/master/youtube-subtitle-selector.js
+// @updateURL    https://raw.githubusercontent.com/Sejmou/userscripts/master/youtube-subtitle-selector.js
 // @grant        none
 // ==/UserScript==
-dispatchKeyboardEvent({ key: 'c' });
-document.querySelector('.ytp-settings-button').click();
-document
-  .querySelector('.ytp-menuitem:nth-of-type(3) .ytp-menuitem-content')
-  .click();
-document.querySelector('[role="menuitemradio"]:nth-of-type(3)').click();
-document.querySelector('[role="menuitemradio"]:nth-of-type(27)').click(); // 27th entry is English
+async function main() {
+  // navigate to https://www.youtube.com/account_playback "Include auto-generated captions (when available)", then the following should work to auto-activate generated English subtitles
+  if (!window.location.href.includes('/watch')) return;
+  document.querySelector('.ytp-settings-button').click();
+  await sleep(100);
+  document
+    .querySelector(
+      '[role="menuitemradio"]:nth-of-type(3) .ytp-menuitem-content'
+    )
+    .click();
+  await sleep(100);
+  document.querySelector('.ytp-settings-button').click();
+  await sleep(100);
+
+  document.querySelector('.ytp-settings-button').click();
+  await sleep(100);
+  document
+    .querySelector('.ytp-menuitem:nth-of-type(3) .ytp-menuitem-content')
+    .click();
+  await sleep(100);
+  document.querySelector('[role="menuitemradio"]:nth-of-type(3)').click();
+  await sleep(100);
+  document.querySelector('[role="menuitemradio"]:nth-of-type(27)').click(); // 27th entry is English
+  await sleep(100);
+  document.querySelector('.ytp-settings-button').click();
+  await sleep(100);
+}
 
 function dispatchKeyboardEvent(kbEvtInit) {
   const keyboardEvent = new KeyboardEvent('keydown', kbEvtInit);
@@ -24,3 +46,9 @@ function dispatchKeyboardEvent(kbEvtInit) {
   document.body.dispatchEvent(keyboardEvent);
   activeElement.focus();
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+setTimeout(main, 2000);
